0d68d53e1f06399fd4b7bb360d500a549c8debe4
Roman Bilyak <25910302+roman-bilyak@users.noreply.github.com>
2023-10-24 22:04:06 +0300

    reset navigation on TabBar item click

--- Chess.Player.MAUI/AppShell.xaml.cs
+++ Chess.Player.MAUI/AppShell.xaml.cs
@@ -6,5 +6,20 @@
         {
             InitializeComponent();
         }
+
+        protected override void OnNavigating(ShellNavigatingEventArgs args)
+        {
+            base.OnNavigating(args);
+
+            if (args.Source == ShellNavigationSource.ShellSectionChanged)
+            {
+                var navigation = Shell.Current.Navigation;
+                var pages = navigation.NavigationStack;
+                for (var i = pages.Count - 1; i >= 1; i--)
+                {
+                    navigation.RemovePage(pages[i]);
+                }
+            }
+        }
     }
 }
